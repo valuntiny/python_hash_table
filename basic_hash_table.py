0f92a3f9f30a643530bb04484565abf046924fcf
valuntiny <459201296@qq.com>
2018-10-05 01:59:49 -0400

    using hash table for leetcode problem

--- basic_hash_table.py
+++ basic_hash_table.py
@@ -1,9 +1,26 @@
-'''
+"""
 author: Guojing Wu
 date: 2018-10-4
 description: this is the basic testing for hash table
-'''
+"""
 
-hash_table = {}
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hash_table = {}
+        for i in nums:
+            try:
+                hash_table.pop(i)
+            except:
+                hash_table[i] = 1
+        return hash_table.popitem()[0]
 
-hash_table.pop(1)
+
+test = Solution()
+
+list1 = [1, 2, 3, 4, 5, 2, 3, 4, 5, 1, 6]
+
+print(test.singleNumber(list1))
